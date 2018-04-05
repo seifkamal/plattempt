9b538610899e013fba0ec932af47df6685234047
safe-k <sf@amp.co>
2018-04-05 11:34:35 +0100

    Initial commit

---
+++ Scenes/Player/Player.gd
@@ -0,0 +1,105 @@
+extends KinematicBody2D
+
+# Environment Physics
+const UP = Vector2(0, -1)
+const GRAVITY = 10
+# Movement
+const ACCELERATION = 20
+const MAX_SPEED = 450
+const JUMP_HEIGHT = 400
+# Blink
+const BLINK_DISTANCE = 110
+
+# Movement
+var motion = Vector2()
+# Blink
+var can_blink = true
+var blink_effect
+var blink_recharge_progress_bar
+
+func _ready():
+	
+	# Blink
+	blink_effect = get_node("/root/Game/BlinkEffect")
+	blink_recharge_progress_bar = $CanvasLayer/BlinkRechargeProgressBar
+	$BlinkTimer.connect("timeout", self, "blink_timer_timeout")
+	
+	print("Player ready")
+	
+	pass
+
+func _process(delta):
+	
+	blink_recharge_progress_bar.value = ($BlinkTimer.time_left * blink_recharge_progress_bar.max_value) / $BlinkTimer.wait_time
+	
+	pass
+
+func _physics_process(delta):
+	
+	# Movement
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		if is_on_floor():
+			$Sprite.animation = "running"
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		if is_on_floor():
+			$Sprite.animation = "running"
+		pass
+	else:
+		motion.x = lerp(motion.x, 0, 0.2)
+		if is_on_floor():
+			$Sprite.animation = "idle"
+			
+	# Jump
+	if Input.is_action_pressed("ui_up"):
+		if is_on_floor():
+			motion.y = -JUMP_HEIGHT
+			$Sprite.animation = "jump"
+		
+	# Blink
+	if Input.is_action_just_pressed("ui_accept"):
+		blink()
+			
+	# Fall
+	if !is_on_floor():
+		motion.y += GRAVITY
+		if motion.y > 0:
+			$Sprite.animation = "fall"
+
+	move_and_slide(motion, UP)
+
+	pass
+
+func blink():
+	
+	var blink_to_pos = self.position
+	
+	if can_blink:
+		if Input.is_action_pressed("ui_left"):
+			blink_to_pos.x -= BLINK_DISTANCE
+		if Input.is_action_pressed("ui_right"):
+			blink_to_pos.x +=  BLINK_DISTANCE
+		if Input.is_action_pressed("ui_up"):
+			blink_to_pos.y -=  BLINK_DISTANCE
+		if Input.is_action_pressed("ui_down"):
+			blink_to_pos.y +=  BLINK_DISTANCE
+		
+	if self.position != blink_to_pos:
+		blink_effect.position = self.position
+		blink_effect.restart()
+		
+		self.position = blink_to_pos
+		
+		can_blink = false
+		$BlinkTimer.start()
+		
+	pass
+	
+func blink_timer_timeout():
+	
+	can_blink = true
+	
+	pass
